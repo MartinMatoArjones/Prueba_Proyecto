391cbda0a6c9a8914f7f2b2a5f2636482814f197
MartinMatoArjones <martin.mato1211@gmail.com>
2024-06-24 14:52:18 +0200

    Inicio

---
+++ build/CMakeFiles/lambda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lambda.dir/main.cpp.o"
+  "CMakeFiles/lambda.dir/main.cpp.o.d"
+  "lambda"
+  "lambda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lambda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
